c9626293ad75fb43b004ac5a666bf0a7e010821c
Ayomide Bakare <bakkareayomideo@gmail.com>
2021-06-16 10:19:13 +0100

    lazy loading + named chunks

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../pages/Home.vue";
-import About from "../pages/About.vue";
-import Store from "../pages/Store.vue";
+
+const Home = () => import(/* webpackChunkName: "home" */ "../pages/Home.vue");
+const About = () =>
+  import(/* webpackChunkName: "about" */ "../pages/About.vue");
+const Store = () =>
+  import(/* webpackChunkName: "store" */ "../pages/Store.vue");
 
 const routerHistory = createWebHistory();
 
@@ -10,14 +13,17 @@ const router = createRouter({
   routes: [
     {
       path: "/",
+      name: "Home",
       component: Home,
     },
     {
       path: "/about",
+      name: "About",
       component: About,
     },
     {
       path: "/store",
+      name: "Store",
       component: Store,
     },
   ],
